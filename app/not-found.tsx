67d891bde5094ba807bbb47a95e827d0bd7db601
Anton Zorkin <zanton1978@gmail.com>
2025-11-09 15:21:20 +0100

    commit message

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 // app/not-found.tsx
 import { Metadata } from 'next';
-import css from './Home.module.css';
+import css from './page.module.css';
 
 export const metadata: Metadata = {
   title: 'Page Not Found',
